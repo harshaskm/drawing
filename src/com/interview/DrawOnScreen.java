ca3203e34c728ad80c310bf7330c28cbcc580b76
Harsha <harshaskm @gmail.com>
2017-09-27 17:37:32 +0800

    Refactored by pulling out methods Created a new method to be able to draw a Horizontal line inside of the canvas

--- src/com/interview/DrawOnScreen.java
+++ src/com/interview/DrawOnScreen.java
@@ -1,40 +1,57 @@
 package com.interview;
 
-import static java.lang.System.*;
-
 public class DrawOnScreen {
 
-    public static char[][] createMatrix(int xaxis, int yaxis) {
+    public static char[][] createCanvas(int xAxis, int yAxis) {
         int horizontalBorder = 2;
-        int modifiedYaxis = yaxis + horizontalBorder;
+        int modifiedYAxis = yAxis + horizontalBorder;
         int verticalBorder = 2;
-        int modifiedXaxis = xaxis + verticalBorder;
-        char[][] box = new char[modifiedYaxis][modifiedXaxis];
+        int modifiedXAxis = xAxis + verticalBorder;
+        char[][] box = new char[modifiedYAxis][modifiedXAxis];
         int headerLine = 0;
-        int footerLine = modifiedYaxis-1;
+        int footerLine = modifiedYAxis-1;
 
-        for (int height = 0; height < modifiedYaxis; height++) {
-            for (int width = 0; width < modifiedXaxis; width++) {
-                box[height][width] = ' ';
-                if (height == headerLine || height == footerLine){
-                    box[height][width] = '-';
-                }
-
-                if ((height > headerLine && height < footerLine) && (width == 0 || width == modifiedXaxis-1 )) {
-                    box[height][width] = '|';
-
-                }
-
-                if ( (height > headerLine && height < footerLine) && (width > 0 && width < modifiedXaxis-1)) {
-                    box[height][width] = ' ';
-                }
-            }
-        }
+        fillMatrix(modifiedYAxis, modifiedXAxis, box, headerLine, footerLine);
 
         return box;
     }
 
-    public static void print2DArray(char[][] box){
+
+    private static void ifVacantInCanvas(int modifiedXAxis, char[][] box, int headerLine, int footerLine, int height, int width) {
+        if ( (height > headerLine && height < footerLine) && (width > 0 && width < modifiedXAxis-1)) {
+            box[height][width] = ' ';
+        }
+    }
+
+    private static void ifBorderCoOrdinates(int modifiedXAxis, char[][] box, int headerLine, int footerLine, int height, int width) {
+        if (height == headerLine || height == footerLine){
+            box[height][width] = '-';
+        }
+
+        if ((height > headerLine && height < footerLine) && (width == 0 || width == modifiedXAxis-1 )) {
+            box[height][width] = '|';
+
+        }
+    }
+
+    private static void fillMatrix(int modifiedYAxis, int modifiedXAxis, char[][] box, int headerLine, int footerLine) {
+        for (int height = 0; height < modifiedYAxis; height++) {
+            for (int width = 0; width < modifiedXAxis; width++) {
+                box[height][width] = ' ';
+
+                ifBorderCoOrdinates(modifiedXAxis, box, headerLine, footerLine, height, width);
+                ifVacantInCanvas(modifiedXAxis, box, headerLine, footerLine, height, width);
+            }
+        }
+    }
+
+    public static void drawHorizontalLineInCanvas(char[][] box, int xCoOrdinate, int yCoOrdinate, int lineLength){
+        for (int xAxis = xCoOrdinate; xAxis <= lineLength; xAxis++) {
+            box[yCoOrdinate][xAxis] = 'x';
+        }
+    }
+
+    public static void printCanvas(char[][] box){
 
         for(char[] row : box) {
             for (char cell : row) {
--- test/TestDrawing.java
+++ test/TestDrawing.java
@@ -1,14 +1,14 @@
 import com.interview.DrawOnScreen;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class TestDrawing {
 
     @Test
     public void printArrayLength(){
-        char[][] box = DrawOnScreen.createMatrix(20, 4);
-        DrawOnScreen.print2DArray(box);
+        char[][] box = DrawOnScreen.createCanvas(20, 4);
+        DrawOnScreen.drawHorizontalLineInCanvas(box, 1, 2, 6);
+        DrawOnScreen.printCanvas(box);
         Assert.assertTrue(true);
     }
 
